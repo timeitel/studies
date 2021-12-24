d8c02b11c0653c54db175773e950789a977374d7
Tim Eitel <timeitel@outlook.com>
2021-12-24 16:54:21 +0800

    feat: ex 1

--- react/react-suspense/src/exercise/01.js
+++ react/react-suspense/src/exercise/01.js
@@ -3,32 +3,24 @@
 
 import * as React from 'react'
 // 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
-import {PokemonDataView} from '../pokemon'
+import {PokemonDataView, fetchPokemon} from '../pokemon'
 
-// 💰 use it like this: fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
+// fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
 
 // 🐨 create a variable called "pokemon" (using let)
-
-// 💣 delete this now...
-const pokemon = {
-  name: 'TODO',
-  number: 'TODO',
-  attacks: {
-    special: [{name: 'TODO', type: 'TODO', damage: 'TODO'}],
-  },
-  fetchedAt: 'TODO',
-}
+let pokemon
 
 // We don't need the app to be mounted to know that we want to fetch the pokemon
 // named "pikachu" so we can go ahead and do that right here.
 // 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-
-// 🐨 when the promise resolves, assign the "pokemon" variable to the resolved value
-// 💰 For example: somePromise.then(resolvedValue => (someValue = resolvedValue))
+const pokemonPromise = fetchPokemon('pikachu').then(
+  resolvedValue => (pokemon = resolvedValue),
+)
 
 function PokemonInfo() {
   // 🐨 if there's no pokemon yet, then throw the pokemonPromise
   // 💰 (no, for real. Like: `throw pokemonPromise`)
+  if (!pokemon) throw pokemonPromise
 
   // if the code gets it this far, then the pokemon variable is defined and
   // rendering can continue!
@@ -46,8 +38,9 @@ function App() {
   return (
     <div className="pokemon-info-app">
       <div className="pokemon-info">
-        {/* 🐨 Wrap the PokemonInfo component with a React.Suspense component with a fallback */}
-        <PokemonInfo />
+        <React.Suspense fallback="loading">
+          <PokemonInfo />
+        </React.Suspense>
       </div>
     </div>
   )
