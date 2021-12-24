d02c70abd41175472611d0a0df9e696943cad05d
Tim Eitel <timeitel@outlook.com>
2021-12-24 17:18:54 +0800

    refactor: ex 01

--- react/react-suspense/src/exercise/01.js
+++ react/react-suspense/src/exercise/01.js
@@ -2,29 +2,13 @@
 // http://localhost:3000/isolated/exercise/01.js
 
 import * as React from 'react'
-// 🐨 you'll also need to get the fetchPokemon function from ../pokemon:
 import {PokemonDataView, fetchPokemon, PokemonErrorBoundary} from '../pokemon'
+import {createResource} from '../utils'
 
-// fetchPokemon(pokemonName).then(handleSuccess, handleFailure)
-
-let pokemon
-let pokemonError
-// We don't need the app to be mounted to know that we want to fetch the pokemon
-// named "pikachu" so we can go ahead and do that right here.
-// 🐨 assign a pokemonPromise variable to a call to fetchPokemon('pikachu')
-const pokemonPromise = fetchPokemon('pikachu').then(
-  resolvedValue => (pokemon = resolvedValue),
-  error => (pokemonError = error),
-)
+const pokemonResource = createResource(fetchPokemon('pikachu'))
 
 function PokemonInfo() {
-  // 🐨 if there's no pokemon yet, then throw the pokemonPromise
-  // � (no, for real. Like: `throw pokemonPromise`)
-  if (pokemonError) throw pokemonError
-  if (!pokemon) throw pokemonPromise
-
-  // if the code gets it this far, then the pokemon variable is defined and
-  // rendering can continue!
+  const pokemon = pokemonResource.read()
   return (
     <div>
       <div className="pokemon-info__img-wrapper">
