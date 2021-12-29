319c53fab87488a4fe2bc4dec2fd20df6997e20e
Tim Eitel <timeitel@outlook.com>
2021-12-29 13:54:02 +0800

    feat: dockerfile

--- dotnet/Catalog/Startup.cs
+++ dotnet/Catalog/Startup.cs
@@ -60,9 +60,9 @@ namespace Catalog
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog v1"));
-            }
 
-            app.UseHttpsRedirection();
+                app.UseHttpsRedirection();
+            }
 
             app.UseRouting();
 
