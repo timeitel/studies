fe9e765c81d19dc98b3d0f5ce027922005c7ef1e
Tim Eitel <timeitel@outlook.com>
2023-01-14 16:14:28 +0800

    wip: string indexing

--- rust/vectors/src/main.rs
+++ rust/vectors/src/main.rs
@@ -13,7 +13,33 @@ fn main() {
     let data = "initial contents".to_string();
     println!("{:?}", data);
 
-    let mut s = String::from("foo");
-    s.push_str("bar");
+    let mut s1 = String::from("foo");
+    let s2 = "bar";
+    s1.push_str(s2); // takes a string slie because we don't want to take ownership of it, i.e.,
+                     // can still print s2 below
+
+    let mut s = String::from("lo");
+    let v = 'l';
+    s.push(v);
     println!("{s}");
+    println!("{v}");
+
+    // add takes ownership of s1,
+    // it appends a copy of the contents of s2 to s1,
+    // and then it returns back ownership of s1.
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2; // note s1 has been moved here and can no longer be used
+    println!("{s3}");
+
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+    let s = format!("{}-{}-{}", s1, s2, s3);
+    println!("{s}")
+
+    // it don't work, can't index strings due to string b eing Vec of bytes, some unicode
+    // characters take more than one byte of storage
+    // let s1 = String::from("hello");
+    // let h = s1[0];
 }
